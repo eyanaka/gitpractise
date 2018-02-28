3c7a6cb93e7bb50f4990fc77cd825779cb679805
Administrator <Administrator@10.0.2.15>
2018-02-28 13:21:55 -0800

    practise for github

---
+++ practisegithub/src/practisegithub/Githubprac.java
@@ -0,0 +1,19 @@
+package practisegithub;
+
+public class Githubprac {
+
+
+private static Githubprac bb;
+public int add(int a,int b) {
+	return(a+b);
+}
+public static void main(String[] args) {
+	Githubprac.bb = new Githubprac();
+	
+	System.out.println(bb.add(20,30));
+	
+	
+}
+
+}
+
